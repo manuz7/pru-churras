03792ec932b57629b3fc301fc40dd98d8998bebd
nmva227 <bassettonavesmanu@gmail.com>
2023-03-09 14:37:14 -0300

    Cara ou Coroa feito

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,21 +1,40 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotatedStated', [
+     state('default', style({transform: 'rotateY(0)'})),
+     state('rotated', style({transform: 'rotateY(-360deg)'})),
+     transition('rotated => default', animate('2000ms ease-out')),
+     transition('rotated => rotated', animate('2000ms ease-in')),
+
+
+
+    ]),
+  ],
 })
 export class Tab1Page {
 
 cara = 'assets/cara.png';
-caroa = 'assets/caroa.png';
+caroa = 'assets/coroa.png';
 logo ='assets/logo.png';
 image = this.logo;
 info = 'Clique no botão para jogar!';
+state = 'default';
 
   constructor() {}
 
   jogarMoeda(){
+    this.image = this.logo;
+    this.info= 'Girando...';
+    this.state = this. state === 'default' ? 'rotated' : 'default';
+
+    setTimeout(()=>{
+
 if(Math.random() < 0.5){
   this.image = this.cara;
   this.info = 'Cara!';
@@ -23,6 +42,8 @@ if(Math.random() < 0.5){
   this.image = this.caroa;
   this.info = 'Coroa!'
 }
+
+}, 2000);
 
   }
 
