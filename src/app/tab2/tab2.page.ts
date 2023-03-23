01585890e8aac47c6af16cda9bf7552120321ede
nmva227 <bassettonavesmanu@gmail.com>
2023-03-23 08:00:26 -0300

    tab2 23/03

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,53 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  numero = 0;
+  time1 = 0;
+  time2 = 0;
+  p1 = 0;
+  p2 = 0;
   constructor() {}
+
+  botoes(valor: number){
+    this.numero = valor;
+  }
+
+  soma1(){
+    this.time1 += this.numero;
+    this.numero = 1;
+    if(this.time1 >=12){
+      this.p1 += 1;
+      this.time1=0;
+      this.time2=0;
+    }
+  }
+  soma2(){
+    this.time2 += this.numero;
+    this.numero = 1;
+    if(this.time2 >=12){
+      this.p2 += 1;
+      this.time1=0;
+      this.time2=0;
+    }
+  }
+
+  tirar1(){
+    this.time1 -= this.numero;
+    this.numero = 1;
+  }
+
+  tirar2(){
+    this.time2 -= this.numero;
+    this.numero = 1;
+  }
+
+  voltar(){
+    this.numero = 0;
+    this.time1 = 0;
+    this.time2 = 0;
+    this.p1=0;
+    this.p2=0;
+  }
+
 
 }
